713e36657651577c91fc593ff05cc55574929f28
jjlee050 <joseph-50@live.com>
2014-09-22 13:43:07 +0800

    View Events Location

---
+++ app/src/main/java/com/fypj/insightsLocal/ui_logic/ViewEventLocationFragment.java
@@ -0,0 +1,60 @@
+package com.fypj.insightsLocal.ui_logic;
+
+/**
+ * Created by L33525 on 22/9/2014.
+ */
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fypj.insightsLocal.R;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fypj.insightsLocal.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ViewEventLocationFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static ViewEventLocationFragment newInstance(int sectionNumber) {
+        ViewEventLocationFragment fragment = new ViewEventLocationFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ViewEventLocationFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_view_event_location, container, false);
+
+        return rootView;
+    }
+}
