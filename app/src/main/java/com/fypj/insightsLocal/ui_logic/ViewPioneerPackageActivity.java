1dc04c7b60ab1b57a267f1d990590e30303c6434
jjlee050 <joseph-50@live.com>
2014-09-25 01:08:57 +0800

    Edit Pioneer Package page

--- app/src/main/java/com/fypj/insightsLocal/ui_logic/ViewPioneerPackageActivity.java
+++ app/src/main/java/com/fypj/insightsLocal/ui_logic/ViewPioneerPackageActivity.java
@@ -1,35 +1,71 @@
 package com.fypj.insightsLocal.ui_logic;
 
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.widget.RelativeLayout;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.fypj.insightsLocal.R;
-import com.fypj.insightsLocal.util.SwipeGestureFilter;
 
-/**
- * Created by jess on 25-Sep-14.
- */
+public class ViewPioneerPackageActivity extends ActionBarActivity implements ActionBar.OnNavigationListener {
 
-public class ViewPioneerPackageActivity extends Activity implements SwipeGestureFilter.SimpleGestureListener {
-    private SwipeGestureFilter detector;
+    /**
+     * The serialization (saved instance state) Bundle key representing the
+     * current dropdown position.
+     */
+    private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pioneer_package);
-        final TextView tvHeader = (TextView) findViewById(R.id.tv_header);
-        final TextView tvContent = (TextView) findViewById(R.id.tv_content);
-        RelativeLayout rlPackage = (RelativeLayout) findViewById(R.id.rl_package);
-        tvHeader.setText("Overview");
-        tvContent.setText("The Government has introduced the Pioneer Generation Package to honour and thank our pioneers for their hard work and dedication. They have made Singapore what it is today. About 450,000 Singaporeans will benefit from the Pioneer Generation Package.");
-        // Detect touched area
-        detector = new SwipeGestureFilter(this,this);
+
+        // Set up the action bar to show a dropdown list.
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        // Show the Up button in the action bar.
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Set up the dropdown list navigation in the action bar.
+        actionBar.setListNavigationCallbacks(
+                // Specify a SpinnerAdapter to populate the dropdown list.
+                new ArrayAdapter<String>(
+                        actionBar.getThemedContext(),
+                        android.R.layout.simple_list_item_1,
+                        android.R.id.text1,
+                        new String[] {
+                                "Introduction",
+                                "Overview",
+                                getString(R.string.title_section3),
+                        }),
+                this);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Restore the previously serialized current dropdown position.
+        if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
+            getSupportActionBar().setSelectedNavigationItem(
+                    savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        // Serialize the current dropdown position.
+        outState.putInt(STATE_SELECTED_NAVIGATION_ITEM,
+                getSupportActionBar().getSelectedNavigationIndex());
     }
 
 
@@ -51,33 +87,58 @@ public class ViewPioneerPackageActivity extends Activity implements SwipeGesture
         }
         return super.onOptionsItemSelected(item);
     }
+
     @Override
-    public boolean dispatchTouchEvent(MotionEvent me){
-        // Call onTouchEvent of SimpleGestureFilter class
-        this.detector.onTouchEvent(me);
-        return super.dispatchTouchEvent(me);
+    public boolean onNavigationItemSelected(int position, long id) {
+        // When the given dropdown item is selected, show its contents in the
+        // container view.
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+        return true;
     }
-    @Override
-    public void onSwipe(int direction) {
-        String str = "";
 
-        switch (direction) {
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
 
-            case SwipeGestureFilter.SWIPE_RIGHT : str = "Swipe Right";
-                break;
-            case SwipeGestureFilter.SWIPE_LEFT :  str = "Swipe Left";
-                break;
-            case SwipeGestureFilter.SWIPE_DOWN :  str = "Swipe Down";
-                break;
-            case SwipeGestureFilter.SWIPE_UP :    str = "Swipe Up";
-                break;
-
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
         }
-        Toast.makeText(this, str, Toast.LENGTH_LONG).show();
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_view_pioneer_package, container, false);
+            TextView tvHeader = (TextView) rootView.findViewById(R.id.tv_header);
+            View horizontalLine = rootView.findViewById(R.id.horizontal_line);
+            TextView tvContent = (TextView) rootView.findViewById(R.id.tv_content);
+            ImageView ivImg = (ImageView) rootView.findViewById(R.id.iv_image);
+
+            tvHeader.setText("CHAS for Pioneer Generation");
+            tvContent.setText("CHAS for Pioneer Generation");
+            horizontalLine.setVisibility(View.GONE);
+            tvContent.setVisibility(View.GONE);
+
+            return rootView;
+        }
     }
 
-    @Override
-    public void onDoubleTap() {
-        Toast.makeText(this, "Double Tap", Toast.LENGTH_SHORT).show();
-    }
 }
